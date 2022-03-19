42a208fc1d422977cb4cf8b2a4d46536d235ce5a
EP-coode <ernestprzybyl@gmail.com>
2022-03-19 17:52:09 +0100

    init lab4 server, implement two methods

---
+++ lab4/server/src/Main.java
@@ -0,0 +1,65 @@
+import org.apache.xmlrpc.WebServer;
+
+import java.util.Vector;
+
+public class Main {
+
+    public Vector<Integer> asyncPrimes(int floor, int celling){
+        int count = 0;
+        int lastPrime = -1;
+        boolean primeFound = false;
+
+        for(int current = floor; current < celling; current++){
+            primeFound = true;
+
+            for(int i = 2; i < current/2; i++){
+                if(current % i == 0){
+                    primeFound = false;
+                    break;
+                }
+            }
+
+            if(primeFound){
+                count++;
+                lastPrime = current;
+            }
+        }
+
+        Vector<Integer> anwser = new Vector<>();
+        anwser.add(count);
+        anwser.add(lastPrime);
+
+        return anwser;
+    }
+
+    private Double distance(double lat1, double lon1, double lat2, double lon2){
+        final double R = 6371d; // radious of earth
+        double latDistanceRads = (lat2 - lat1) * Math.PI / 180;
+        double lonDistanceRads = (lon2 - lon1) * Math.PI / 180;
+        double a = Math.sin(latDistanceRads / 2) * Math.sin(latDistanceRads / 2) +
+                Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
+                        Math.sin(lonDistanceRads / 2) * Math.sin(lonDistanceRads / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+        return R * c;
+    }
+
+
+
+    public static void main(String[] args) {
+        try{
+            System.err.println("Startuje serwer XML-RPC...");
+            int port = 4000;
+            WebServer server = new WebServer(port);
+            server.addHandler("MojSerwer", new Main());
+            server.start();
+            System.out.println("Serwer wystartował pomślnie");
+            System.out.println("Nasłuchuje na porcie: " + port);
+            System.out.println("Aby zatrzymać naciśnij ctrl+c");
+        }
+
+        catch (Exception exception){
+            System.err.println("Serwer XML-RPC: "+ exception);
+        }
+    }
+}
