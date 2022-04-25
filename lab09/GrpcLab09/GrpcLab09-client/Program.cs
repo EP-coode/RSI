8c6599b07a6b62eb84016e772095bed1721d410c
Ernest Przybyl <ernestprzybyl@gmail.com>
2022-04-25 08:02:23 +0200

    add myData

--- lab09/GrpcLab09/GrpcLab09-client/Program.cs
+++ lab09/GrpcLab09/GrpcLab09-client/Program.cs
@@ -1,46 +1,78 @@
 ﻿using System;
+using System.Net;
 using System.Threading.Tasks;
 using Grpc.Net.Client;
 
 namespace GrpcLab09_client
 {
+    class MyData
+    {
+        public static void info()
+        {
+            Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+            Console.WriteLine("Ernest Przybył 256480");
+            Console.WriteLine(Environment.UserName);
+            Console.WriteLine(System.Environment.OSVersion.VersionString);
+            Console.WriteLine(Environment.Version);
+            Console.WriteLine(Dns.GetHostByName(Dns.GetHostName()).AddressList[2].ToString());
+        }
+    }
     internal class Program
     {
         static async Task Main(string[] args)
         {
+            MyData.info();
             Console.WriteLine("Starting gRPC Client");
             var chanel = GrpcChannel.ForAddress("https://localhost:5001");
             var client = new Kociarnia.KociarniaClient(chanel);
-            Console.WriteLine("Enter the name: ");
-            string str = Console.ReadLine();
-            int val = 21;
+
             var reply = await client.DodajKotkaAsync(new Kotek()
             {
                 Id = 1,
                 Nazwa = "Kitek",
                 Waga = 5,
             });
+
             Console.WriteLine($"From server: {reply.Msg}");
             reply = await client.DodajKotkaAsync(new Kotek()
             {
                 Id = 2,
                 Nazwa = "Tygrys",
-                Waga = 6,
+                Waga = 6.5,
             });
             Console.WriteLine($"From server: {reply.Msg}");
 
-            reply = await client.DodajKotkaAsync(new Kotek()
+            var kotKajtek = new Kotek()
             {
-                Id = 2,
-                Nazwa = "Tygrys",
-                Waga = 6,
-            });
+                Id = 3,
+                Nazwa = "Kajtek",
+                Waga = 6.3,
+            };
+
+            reply = await client.DodajKotkaAsync(kotKajtek);
             Console.WriteLine($"From server: {reply.Msg}");
 
 
             ListaKotow l = await client.PodajKotkiAsync(new ParametryWyszukiwaniaKotkow());
+            Console.WriteLine("-----------------lista kotków ---------------:");
 
-            foreach(var kotek in l.Kotki)
+            foreach (var kotek in l.Kotki)
+            {
+                Console.WriteLine($"Kot {kotek.Nazwa} ma wagę, {kotek.Waga}");
+            }
+
+            await client.UsunKotkaAsync(kotKajtek);
+            await client.EdytujKotkaAsync(new Kotek()
+            {
+                Id = 1,
+                Nazwa = "Kitek",
+                Waga = 10.5
+            });
+
+            Console.WriteLine("-----------------Po usunięciu kajtka i edycji kitka ---------------:");
+            l = await client.PodajKotkiAsync(new ParametryWyszukiwaniaKotkow());
+
+            foreach (var kotek in l.Kotki)
             {
                 Console.WriteLine($"Kot {kotek.Nazwa} ma wagę, {kotek.Waga}");
             }
--- lab09/GrpcLab09/GrpcLab09/Program.cs
+++ lab09/GrpcLab09/GrpcLab09/Program.cs
@@ -4,14 +4,29 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace GrpcLab09
 {
+    class MyData
+    {
+        public static void info()
+        {
+            Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+            Console.WriteLine("Ernest Przyby³ 256480");
+            Console.WriteLine(Environment.UserName);
+            Console.WriteLine(System.Environment.OSVersion.VersionString);
+            Console.WriteLine(Environment.Version);
+            Console.WriteLine(Dns.GetHostByName(Dns.GetHostName()).AddressList[2].ToString());
+        }
+    }
+
     public class Program
     {
         public static void Main(string[] args)
         {
+            MyData.info();
             CreateHostBuilder(args).Build().Run();
         }
 
