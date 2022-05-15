efeca2e8f275225d78971ef4f9e5792a7201a00a
EP-coode <ernestprzybyl@gmail.com>
2022-05-15 15:17:30 +0200

    start implementig client

---
+++ lab10/Zadanie4_Etap2/KlientWcf/KotkiCallbackHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KlientWcf.KotkiServiceRef;
+
+namespace KlientWcf
+{
+    internal class KotkiCallbackHandler : IKotkiServiceCallback
+    {
+        public void AddKotekResult(Kotek[] kotki)
+        {
+            Console.WriteLine("Pomyślnie dodano.");
+        }
+
+        public void DeleteKotekResult(Kotek usunietyKotek)
+        {
+            Console.WriteLine("Usunięto kota: ");
+            Console.WriteLine(usunietyKotek);
+        }
+
+        public void GetKotkiByPlec(Kotek[] kotki)
+        {
+            Console.WriteLine("Wynik: ");
+            foreach (var kotek in kotki)
+                Console.WriteLine(kotek);
+        }
+
+        public void GetKotkiResult(Kotek[] kotki)
+        {
+            Console.WriteLine("Wszyskie koty: ");
+            foreach (var kotek in kotki)
+                Console.WriteLine(kotek);
+        }
+    }
+}
---
+++ lab10/Zadanie4_Etap2/KlientWcf/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using KlientWcf.KotkiServiceRef;
+
+namespace KlientWcf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            KotkiCallbackHandler callback = new KotkiCallbackHandler();
+            InstanceContext instanceContext = new InstanceContext(callback);
+            KotkiServiceClient client = new KotkiServiceClient(instanceContext);
+
+
+            Dictionary<int, Action> actions = new Dictionary<int, Action>()
+            {
+                { 1, () => { client.GetKotki(); } },
+                { 2, () => { client.GetKotkiByGender(); } },
+                { 3, () => { } },
+                { 4, () => { } },
+                { 5, () => { } },
+            };
+
+            while (true)
+            {
+                PrintActions();
+                int selectedAction;
+                int.TryParse(Console.ReadLine(), out selectedAction);
+
+                if (actions.ContainsKey(selectedAction))
+                {
+                    actions[selectedAction]();
+                    Console.WriteLine("Poczekaj na wynik...");
+                }
+            }
+        }
+
+        static void PrintActions()
+        {
+            Console.WriteLine("Wybierz akcje: ");
+            Console.WriteLine("1. Podaj wszyskie koty");
+            Console.WriteLine("2. Podaj wszyskie kotki");
+            Console.WriteLine("3. Podaj wszyskie kocury");
+            Console.WriteLine("4. Dodaj kota");
+            Console.WriteLine("5. Usun kota");
+        }
+    }
+}
